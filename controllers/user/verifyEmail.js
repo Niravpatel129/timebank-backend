3e6dd15c27f067b05ba2b901c5417c385cdc3893
Nirav <niravpatelp129@gmail.com>
2024-08-10 14:28:56 -0400

    * refactor(user): removed unnecessary comments

--- controllers/user/verifyEmail.js
+++ controllers/user/verifyEmail.js
@@ -4,13 +4,11 @@ const verifyEmail = async (req, res) => {
   try {
     const { email, verificationCode } = req.body;
 
-    // find email + verification code
     const user = await User.findOne({ email, verificationCode }).select('-password');
     if (!user) {
       return res.status(400).json({ message: 'Invalid verification code' });
     }
 
-    // update user emailVerified to true
     user.emailVerified = true;
     await user.save();
 
