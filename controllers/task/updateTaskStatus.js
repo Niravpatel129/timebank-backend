6f7788c7e1345b52310a6f1f778e8d5140fa304b
Nirav <niravpatelp129@gmail.com>
2024-08-12 02:42:24 -0400

    * fix(controllers): added missing semicolon

--- controllers/task/updateTaskStatus.js
+++ controllers/task/updateTaskStatus.js
@@ -6,11 +6,14 @@ const updateTaskStatus = async (req, res) => {
     const { status } = req.body;
     const userId = req.user.id; // Assuming user ID is available in req.user after authentication
 
-    const task = await Task.findOne({ _id: taskId, user: userId });
+    const task = await Task.findOne({ _id: taskId, user: userId })
+      .populate('user', 'name email')
+      .populate('assignee', 'name email');
 
     if (!task) {
       return res.status(404).json({
         success: false,
+        task: task,
         message: 'Task not found or you do not have permission to update this task',
       });
     }
